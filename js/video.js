c96e1086352d006a4674e82c478c30058ebe711a
JhonatanChumbes <68823446+JhonatanChumbes@users.noreply.github.com>
2020-07-26 14:08:49 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,180 @@
+const video_1=document.getElementById('video_1');
+const play_1=document.getElementById('play_1');
+const pausa_1=document.getElementById('pausa_1');
+play_1.addEventListener('click',()=>{
+    video_1.play()
+})
+pausa_1.addEventListener('click',()=>{
+    video_1.pause()
+})
+const video_2=document.getElementById('video_2');
+const play_2=document.getElementById('play_2');
+const pausa_2=document.getElementById('pausa_2');
+play_2.addEventListener('click',()=>{
+    video_2.play()
+})
+pausa_2.addEventListener('click',()=>{
+    video_2.pause()
+})
+const video_3=document.getElementById('video_3');
+const play_3=document.getElementById('play_3');
+const pausa_3=document.getElementById('pausa_3');
+play_3.addEventListener('click',()=>{
+    video_3.play()
+})
+pausa_3.addEventListener('click',()=>{
+    video_3.pause()
+})
+const video_4=document.getElementById('video_4');
+const play_4=document.getElementById('play_4');
+const pausa_4=document.getElementById('pausa_4');
+play_4.addEventListener('click',()=>{
+    video_4.play()
+})
+pausa_4.addEventListener('click',()=>{
+    video_4.pause()
+})
+const video_5=document.getElementById('video_5');
+const play_5=document.getElementById('play_5');
+const pausa_5=document.getElementById('pausa_5');
+play_5.addEventListener('click',()=>{
+    video_5.play()
+})
+pausa_5.addEventListener('click',()=>{
+    video_5.pause()
+})
+const video_6=document.getElementById('video_6');
+const play_6=document.getElementById('play_6');
+const pausa_6=document.getElementById('pausa_6');
+play_6.addEventListener('click',()=>{
+    video_6.play()
+})
+pausa_6.addEventListener('click',()=>{
+    video_6.pause()
+})
+const video_7=document.getElementById('video_7');
+const play_7=document.getElementById('play_7');
+const pausa_7=document.getElementById('pausa_7');
+play_7.addEventListener('click',()=>{
+    video_7.play()
+})
+pausa_7.addEventListener('click',()=>{
+    video_7.pause()
+})
+const video_8=document.getElementById('video_8');
+const play_8=document.getElementById('play_8');
+const pausa_8=document.getElementById('pausa_8');
+play_8.addEventListener('click',()=>{
+    video_8.play()
+})
+pausa_8.addEventListener('click',()=>{
+    video_8.pause()
+})
+const video_9=document.getElementById('video_9');
+const play_9=document.getElementById('play_9');
+const pausa_9=document.getElementById('pausa_9');
+play_9.addEventListener('click',()=>{
+    video_9.play()
+})
+pausa_9.addEventListener('click',()=>{
+    video_9.pause()
+})
+const video_10=document.getElementById('video_10');
+const play_10=document.getElementById('play_10');
+const pausa_10=document.getElementById('pausa_10');
+play_10.addEventListener('click',()=>{
+    video_10.play()
+})
+pausa_10.addEventListener('click',()=>{
+    video_10.pause()
+})
+const video_11=document.getElementById('video_11');
+const play_11=document.getElementById('play_11');
+const pausa_11=document.getElementById('pausa_11');
+play_11.addEventListener('click',()=>{
+    video_11.play()
+})
+pausa_11.addEventListener('click',()=>{
+    video_11.pause()
+})
+const video_12=document.getElementById('video_12');
+const play_12=document.getElementById('play_12');
+const pausa_12=document.getElementById('pausa_12');
+play_12.addEventListener('click',()=>{
+    video_12.play()
+})
+pausa_12.addEventListener('click',()=>{
+    video_12.pause()
+})
+const video_13=document.getElementById('video_13');
+const play_13=document.getElementById('play_13');
+const pausa_13=document.getElementById('pausa_13');
+play_13.addEventListener('click',()=>{
+    video_13.play()
+})
+pausa_13.addEventListener('click',()=>{
+    video_13.pause()
+})
+const video_14=document.getElementById('video_14');
+const play_14=document.getElementById('play_14');
+const pausa_14=document.getElementById('pausa_14');
+play_14.addEventListener('click',()=>{
+    video_14.play()
+})
+pausa_14.addEventListener('click',()=>{
+    video_14.pause()
+})
+const video_15=document.getElementById('video_15');
+const play_15=document.getElementById('play_15');
+const pausa_15=document.getElementById('pausa_15');
+play_15.addEventListener('click',()=>{
+    video_15.play()
+})
+pausa_15.addEventListener('click',()=>{
+    video_15.pause()
+})
+const video_16=document.getElementById('video_16');
+const play_16=document.getElementById('play_16');
+const pausa_16=document.getElementById('pausa_16');
+play_16.addEventListener('click',()=>{
+    video_16.play()
+})
+pausa_16.addEventListener('click',()=>{
+    video_16.pause()
+})
+const video_17=document.getElementById('video_17');
+const play_17=document.getElementById('play_17');
+const pausa_17=document.getElementById('pausa_17');
+play_17.addEventListener('click',()=>{
+    video_17.play()
+})
+pausa_17.addEventListener('click',()=>{
+    video_17.pause()
+})
+const video_18=document.getElementById('video_18');
+const play_18=document.getElementById('play_18');
+const pausa_18=document.getElementById('pausa_18');
+play_18.addEventListener('click',()=>{
+    video_18.play()
+})
+pausa_18.addEventListener('click',()=>{
+    video_18.pause()
+})
+const video_19=document.getElementById('video_19');
+const play_19=document.getElementById('play_19');
+const pausa_19=document.getElementById('pausa_19');
+play_19.addEventListener('click',()=>{
+    video_19.play()
+})
+pausa_19.addEventListener('click',()=>{
+    video_19.pause()
+})
+const video_20=document.getElementById('video_20');
+const play_20=document.getElementById('play_20');
+const pausa_20=document.getElementById('pausa_20');
+play_20.addEventListener('click',()=>{
+    video_20.play()
+})
+pausa_20.addEventListener('click',()=>{
+    video_20.pause()
+})
---
+++ js/video_1.js
@@ -0,0 +1,90 @@
+const video_1=document.getElementById('video_1');
+const play_1=document.getElementById('play_1');
+const pausa_1=document.getElementById('pausa_1');
+play_1.addEventListener('click',()=>{
+    video_1.play()
+})
+pausa_1.addEventListener('click',()=>{
+    video_1.pause()
+})
+const video_2=document.getElementById('video_2');
+const play_2=document.getElementById('play_2');
+const pausa_2=document.getElementById('pausa_2');
+play_2.addEventListener('click',()=>{
+    video_2.play()
+})
+pausa_2.addEventListener('click',()=>{
+    video_2.pause()
+})
+const video_3=document.getElementById('video_3');
+const play_3=document.getElementById('play_3');
+const pausa_3=document.getElementById('pausa_3');
+play_3.addEventListener('click',()=>{
+    video_3.play()
+})
+pausa_3.addEventListener('click',()=>{
+    video_3.pause()
+})
+const video_4=document.getElementById('video_4');
+const play_4=document.getElementById('play_4');
+const pausa_4=document.getElementById('pausa_4');
+play_4.addEventListener('click',()=>{
+    video_4.play()
+})
+pausa_4.addEventListener('click',()=>{
+    video_4.pause()
+})
+const video_5=document.getElementById('video_5');
+const play_5=document.getElementById('play_5');
+const pausa_5=document.getElementById('pausa_5');
+play_5.addEventListener('click',()=>{
+    video_5.play()
+})
+pausa_5.addEventListener('click',()=>{
+    video_5.pause()
+})
+const video_6=document.getElementById('video_6');
+const play_6=document.getElementById('play_6');
+const pausa_6=document.getElementById('pausa_6');
+play_6.addEventListener('click',()=>{
+    video_6.play()
+})
+pausa_6.addEventListener('click',()=>{
+    video_6.pause()
+})
+const video_7=document.getElementById('video_7');
+const play_7=document.getElementById('play_7');
+const pausa_7=document.getElementById('pausa_7');
+play_7.addEventListener('click',()=>{
+    video_7.play()
+})
+pausa_7.addEventListener('click',()=>{
+    video_7.pause()
+})
+const video_8=document.getElementById('video_8');
+const play_8=document.getElementById('play_8');
+const pausa_8=document.getElementById('pausa_8');
+play_8.addEventListener('click',()=>{
+    video_8.play()
+})
+pausa_8.addEventListener('click',()=>{
+    video_8.pause()
+})
+const video_9=document.getElementById('video_9');
+const play_9=document.getElementById('play_9');
+const pausa_9=document.getElementById('pausa_9');
+play_9.addEventListener('click',()=>{
+    video_9.play()
+})
+pausa_9.addEventListener('click',()=>{
+    video_9.pause()
+})
+const video_10=document.getElementById('video_10');
+const play_10=document.getElementById('play_10');
+const pausa_10=document.getElementById('pausa_10');
+play_10.addEventListener('click',()=>{
+    video_10.play()
+})
+pausa_10.addEventListener('click',()=>{
+    video_10.pause()
+})
